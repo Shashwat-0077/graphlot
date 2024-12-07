dbb70472a4cb8dc8f6a1a95b4e016d8df95d180c
Shashwat <shashwatg0911@gmail.com>
2024-12-07 17:00:00 +0530

    Try number 2 for notion embed

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,12 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: "/(.*)",
+                source: "/:path*",
                 headers: [
-                    {
-                        key: "X-Frame-Options",
-                        value: "ALLOWALL",
-                    },
+                    { key: "X-Frame-Options", value: "ALLOWALL" },
                     {
                         key: "Content-Security-Policy",
-                        value: "frame-ancestors 'self' https://www.notion.so;",
+                        value: "frame-ancestors *;",
                     },
                 ],
             },
