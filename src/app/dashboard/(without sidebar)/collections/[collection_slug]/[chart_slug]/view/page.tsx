b75d961af701d200c3ec297d98264b2e8eac98cb
Shashwat <shashwatg0911@gmail.com>
2025-05-03 22:40:03 +0530

    This and followings commits will be for notion display/embeddinbg testing

---
+++ src/app/dashboard/(without sidebar)/collections/[collection_slug]/[chart_slug]/view/page.tsx
@@ -0,0 +1,85 @@
+"use client";
+import { use } from "react";
+
+import { ChartViewComponentType, StateProviderType } from "@/constants";
+import { AreaChartView } from "@/modules/Area/components/AreaChartView";
+import { AreaChartStoreProvider } from "@/modules/Area/store";
+import { BarChartView } from "@/modules/Bar/components/BarChartView";
+import { BarChartStoreProvider } from "@/modules/Bar/store";
+import { useGetChartWithId } from "@/modules/BasicChart/api/client/useGetChart";
+import { DonutChartView } from "@/modules/Donut/components/DonutChartView";
+import { DonutChartStoreProvider } from "@/modules/Donut/store";
+import { HeatmapChartView } from "@/modules/Heatmap/components/HeatmapChartView";
+import { HeatmapChartStoreProvider } from "@/modules/Heatmap/store";
+import { RadarChartView } from "@/modules/Radar/components/RadarChartView";
+import { RadarChartStoreProvider } from "@/modules/Radar/store";
+import { parseSlug } from "@/utils/pathSlugsOps";
+import { BoxLoader } from "@/components/ui/Loader";
+
+type Props = {
+    params: Promise<{
+        chart_slug: string;
+        collection_slug: string;
+    }>;
+};
+
+export default function ChatConfigs({ params }: Props) {
+    const { chart_slug, collection_slug } = use(params);
+
+    const { id: chart_id } = parseSlug(chart_slug);
+    const { id: collection_id } = parseSlug(collection_slug);
+
+    const {
+        data: chartData,
+        isLoading: isChartLoading,
+        error: chartError,
+    } = useGetChartWithId(chart_id);
+
+    if (!chart_id) {
+        return <div>Invalid Chart ID</div>;
+    }
+
+    if (!collection_id) {
+        return <div>Invalid Collection ID</div>;
+    }
+
+    if (isChartLoading) {
+        return (
+            <div className="flex h-screen w-full items-center justify-center">
+                <BoxLoader />
+            </div>
+        );
+    }
+
+    if (chartError || !chartData) {
+        return <div>Error: {chartError?.message}</div>;
+    }
+
+    const { chart } = chartData;
+
+    const chartComponents: {
+        [key: string]: [ChartViewComponentType, StateProviderType];
+    } = {
+        Bar: [BarChartView, BarChartStoreProvider],
+        Radar: [RadarChartView, RadarChartStoreProvider],
+        Area: [AreaChartView, AreaChartStoreProvider],
+        Donut: [DonutChartView, DonutChartStoreProvider],
+        Heatmap: [HeatmapChartView, HeatmapChartStoreProvider],
+    };
+
+    const [ChartView, StoreProvider] = chartComponents[chart.type] || [
+        RadarChartView,
+        RadarChartStoreProvider,
+    ];
+
+    return (
+        <div className="flex h-screen w-full items-center justify-center">
+            <StoreProvider char_id={chart_id}>
+                <ChartView
+                    notion_table_id={chart.notion_database_id}
+                    chartName={chart.name}
+                />
+            </StoreProvider>
+        </div>
+    );
+}
